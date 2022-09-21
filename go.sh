cdb3bb24474f0991e9f6075ed50bc27650c7d0e4
Omer Tuchfeld <omer@tuchfeld.dev>
2022-09-21 15:29:29 +0100

    Typo

--- go.sh
+++ go.sh
@@ -6,5 +6,5 @@ SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
 
 $SCRIPT_DIR/ignition_dump.py bootstrap-in-place-for-live-iso.ign
 
-cp ign-root/opt/openshift/openshift/99-openshift-master-storage.yaml .
-cp ign-root/opt/openshift/openshift/99-openshift-worker-storage.yaml .
+cp $SCRIPT_DIR/ign-root/opt/openshift/openshift/99-openshift-master-storage.yaml $SCRIPT_DIR/
+cp $SCRIPT_DIR/ign-root/opt/openshift/openshift/99-openshift-worker-storage.yaml $SCRIPT_DIR/
